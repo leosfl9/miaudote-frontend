e3dccc833932adbe97ec29756147871ca913436a
leosfl9 <leonardosilvaflores29@gmail.com>
2025-09-27 16:43:08 -0300

    fix: classes duplicadas no botao de form

--- components/FormButton.tsx
+++ components/FormButton.tsx
@@ -12,7 +12,7 @@ export default function FormButton({ text, color, onClick, className, ...props }
     <button
       onClick={onClick}
       className={`
-        w-full text-xl py-1 rounded-[48px] font-medium transition-colors text-white font-semibold cursor-pointer shadow-[0_4px_4px_rgba(0,0,0,0.25)]
+        w-full text-xl py-1 rounded-[48px] transition-colors text-white font-semibold cursor-pointer shadow-[0_4px_4px_rgba(0,0,0,0.25)]
         ${color === "green" && "bg-miau-green hover:bg-miau-light-green active:bg-miau-light-green"}
         ${className}
       `}
