9f55c8a15798e7719a8c986ed7615a9f0d6bd942
leosfl9 <leonardosilvaflores29@gmail.com>
2025-10-04 21:27:09 -0300

    feat: tela de home de parceiros (sem integracao)

--- app/(parceiro)/parceiro/home/page.tsx
+++ app/(parceiro)/parceiro/home/page.tsx
@@ -1,7 +1,28 @@
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
+import AnimalCard from "@/components/AnimalCard";
+
 export default function homeParceiro(){
     return (
-        <div>
-            
+        <div className="flex flex-col gap-6 px-4 sm:px-16 md:px-20 lg:px-30 py-4 sm:py-8 lg:py-10 ">
+            <div className="flex flex-col gap-2 text-text-light-gray">
+                <h1 className="font-bold text-3xl">Pets cadastrados</h1>
+                <h2 className="text-base">Cadastre novos pets e verifique os já cadastrados para adoção.</h2>
+            </div>
+
+            <div className="flex flex-col lg:flex-row lg:flex-wrap gap-3 lg:gap-6 items-center lg:items-start">
+                <Link href={"/parceiro/cadastro-pet"} className="bg-[#C09BD84D] hover:bg-miau-purple active:bg-miau-purple/80 
+                    text-text-gray hover:text-background active:text-background flex gap-2 items-center px-4 py-2 rounded-xl w-full max-w-[380px]
+                    lg:h-[512px] lg:flex-col lg:items-center lg:justify-center lg:gap-6">
+                    <CirclePlus className="w-8 lg:w-24 h-8 lg:h-24" />
+                    <h3 className="font-semibold text-lg lg:text-2xl">Adicionar novo pet</h3>
+                </Link>
+
+                <AnimalCard />
+                <AnimalCard />
+                <AnimalCard />
+
+            </div>
         </div>
     );
 }
---
+++ components/AnimalCard.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { CalendarDays, Ruler } from "lucide-react";
+import Link from "next/link";
+
+export default function AnimalCard(){
+    return(
+        <div className="flex flex-col max-w-[380px] bg-white rounded-xl">
+            <div className="relative w-full min-h-64">
+                <Image alt="Imagem do animal" src={"/Bolt.jpeg"} className="rounded-t-xl object-cover object-center" fill/>
+            </div>
+
+            <div className="flex flex-col gap-8 px-5 py-5">
+                <div className="flex flex-col gap-4">
+                    <h4 className="text-text-light-gray text-2xl font-bold">Bolt</h4>
+                    <div className="flex flex-wrap gap-4 text-[#7B7B7B] items-center">
+                        <div className="flex gap-[6px] items-center">
+                            <Image alt="Ícone de cachorro" src={"/icon_cao.png"} width={20} height={20} />
+                            <p className="text-sm font-medium">Cão</p>
+                        </div>
+
+                        <div className="flex gap-[6px] items-center">
+                            <CalendarDays className="w-5 h-5" />
+                            <p className="text-sm font-medium">6 anos</p>
+                        </div>
+
+                        <div className="flex gap-[6px] items-center">
+                            <Ruler className="w-5 h-5" />
+                            <p className="text-sm font-medium">Pequeno</p>
+                        </div>
+                    </div>
+                    <p className="text-sm text-[#7B7B7B] text-justify line-clamp-3">
+                        Thor é um cachorro muito brincalhão e cheio de energia. Adora passeios e se dá bem com crianças e outros animais. 
+                    </p>
+                </div>
+
+                <Link href={"/"} className={`px-4 py-2 rounded-4xl shadow-[0_4px_4px_rgba(0,0,0,0.25)] transition w-fit 
+                    bg-miau-orange text-background hover:bg-miau-green active:bg-miau-light-green font-bold`}>
+                    Visualizar
+                </Link>
+            </div>
+        </div>
+    );
+}
