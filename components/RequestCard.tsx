a4e29f63d659fe5445c41b5292deea3980adaff7
leosfl9 <leonardosilvaflores29@gmail.com>
2025-10-07 13:52:16 -0300

    feat: tela de detalhes de solicitacao de adocao

--- components/RequestCard.tsx
+++ components/RequestCard.tsx
@@ -18,9 +18,9 @@ export default function RequestCard () {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col sm:flex-row gap-3 sm:gap-8 sm:items-center text-center">
+            <div className="flex flex-col sm:flex-row gap-3 sm:gap-8 sm:items-center text-center pb-1">
                 <h2 className="text-text-gray text-sm">Solicitado em: <span>10/11/2025</span></h2>
-                <Link href={"/parceiro/solicitacoes"} className={`px-4 py-2 rounded-4xl shadow-[0_4px_4px_rgba(0,0,0,0.25)] transition w-full sm:w-fit
+                <Link href={"/parceiro/detalhes-solicitacao"} className={`px-4 py-2 rounded-4xl shadow-[0_4px_4px_rgba(0,0,0,0.25)] transition w-full sm:w-fit
                 bg-miau-orange text-background hover:bg-miau-green active:bg-miau-light-green font-bold`}>
                     Ver detalhes
                 </Link>
