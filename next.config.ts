e83e6d342fa12c105f9ea353a704a936367dfe88
leosfl9 <leonardosilvaflores29@gmail.com>
2025-10-25 12:13:40 -0300

    deploy: vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   eslint: {
+  output: "standalone",
+  eslint: {
     ignoreDuringBuilds: true,
   },
 };
