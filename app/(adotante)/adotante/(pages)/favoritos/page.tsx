a70cd87c9d2b5861e13fa7a6acd029e0f84c8111
leosfl9 <leonardosilvaflores29@gmail.com>
2025-10-30 12:39:12 -0300

    style: centralizando cards para telas menores

--- app/(adotante)/adotante/(pages)/favoritos/page.tsx
+++ app/(adotante)/adotante/(pages)/favoritos/page.tsx
@@ -257,12 +257,12 @@ export default function homeAdotante(){
 
     return (
         <div className="flex flex-col gap-6 px-4 sm:px-16 md:px-20 lg:px-30 py-4 sm:py-8 lg:py-10 ">
-            <div className="flex flex-col gap-2 text-text-light-gray">
+            <div className="flex flex-col gap-2 text-text-light-gray text-center">
                 <h1 className="font-bold text-3xl 2xl:text-4xl">Meus favoritos</h1>
                 <h2 className="text-base 2xl:text-lg">Visualize  seus favoritos, toda beleza do mundo em um só lugar!</h2>
             </div>
 
-            <div className="flex flex-col lg:flex-row lg:flex-wrap gap-3 lg:gap-6 items-center lg:items-start">
+            <div className="flex flex-col lg:flex-row lg:flex-wrap gap-3 lg:gap-6 items-center lg:items-start justify-center">
                 {/* se houverem animais, exibe-os */}
                 {animais.length > 0 ? (
                     animais.map((animal) => (
